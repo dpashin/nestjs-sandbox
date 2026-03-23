d6781a9fc92411cf6a717447e0b490c3c1ae5e38
Dmitriy Pashin <Dmitriy.Pashin@ya.ru>
2026-03-23 18:03:52 +0700

    feat: yolo docker-compose + db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoanModule } from './loan/loan.module';
 
 @Module({
-  imports: [LoanModule],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: parseInt(process.env.DATABASE_PORT || '5432', 10),
+      username: process.env.DATABASE_USER || 'postgres',
+      password: process.env.DATABASE_PASSWORD || 'postgres',
+      database: process.env.DATABASE_NAME || 'postgres',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    LoanModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
